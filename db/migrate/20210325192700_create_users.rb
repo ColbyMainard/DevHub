ea31bc7c15fc18729872557987a7bf8fb6c7a8fc
ColbyMainard <55711813+ColbyMainard@users.noreply.github.com>
2021-03-25 15:28:08 -0500

    Fixes to database

---
+++ db/migrate/20210325192700_create_users.rb
@@ -0,0 +1,17 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :profile_picture_link
+      t.string :username
+      t.string :email
+      t.string :password
+      t.string :discord_username
+      t.string :snapchat_username
+      t.string :instagram_handle
+      t.string :github_link
+      t.boolean :is_admin
+
+      t.timestamps
+    end
+  end
+end
