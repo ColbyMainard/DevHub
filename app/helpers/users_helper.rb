36469cc1f60e7fe2a610985fb45928d895f4fcbc
ColbyMainard <cmythman@gmail.com>
2021-03-26 18:17:07 -0500

    Update to helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -10,14 +10,37 @@ module UsersHelper
             is_uppercase_letter = uppercase.include? name[index]
             is_lowercase_letter = lowercase.include? name[index]
             is_digit = numeric.include? name[index]
-            if not (is_digit || is_lowercase_letter || is_uppercase_letter)
+            is_underscore = underscore.include? name[index]
+            if not (is_digit || is_lowercase_letter || is_uppercase_letter || is_underscore)
                 return false
             end
         end
         return true
     end
     def valid_password? password
+        for index in 0..name.length
+            is_uppercase_letter = uppercase.include? name[index]
+            is_lowercase_letter = lowercase.include? name[index]
+            is_digit = numeric.include? name[index]
+            is_underscore = underscore.include? name[index]
+            is_special = valid_special_characters.include? name[index]
+            if not (is_digit || is_lowercase_letter || is_uppercase_letter || is_underscore)
+                return false
+            end
+        end
+        return true
     end
     def valid_social? social_handle
+        for index in 0..name.length
+            is_uppercase_letter = uppercase.include? name[index]
+            is_lowercase_letter = lowercase.include? name[index]
+            is_digit = numeric.include? name[index]
+            is_underscore = underscore.include? name[index]
+            is_special = valid_special_characters.include? name[index]
+            if not (is_digit || is_lowercase_letter || is_uppercase_letter || is_underscore)
+                return false
+            end
+        end
+        return true
     end
 end
