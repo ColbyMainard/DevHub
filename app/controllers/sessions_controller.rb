d145c48499938b0be154256c4fb04fbeb55436ad
ColbyMainard <cmythman@gmail.com>
2021-04-06 08:27:27 -0500

    Adding node modules

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
     def new
     end
     def create
-        user = User.find_by_email(params[:email])
+        user = User.find_by_email(params[:username])
         if user && user.authenticate(params[:password])
             session[:user_id] = user.id
             redirect_to root_url, notice: "Logged in!"
