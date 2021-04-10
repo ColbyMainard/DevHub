140cf263d3221ac49d8683483afb59b090cd7b83
EC2 Default User <ec2-user@ip-172-31-12-14.us-east-2.compute.internal>
2021-04-10 17:43:27 +0000

    added fixes

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,9 @@ Rails.application.routes.draw do
   get '/users/new', to: 'users#new', as: 'new_user'
   get '/users/:id', to: 'users#show', as: 'show_user'
   get '/users', to: 'users#index', as: 'list_users'
-  get '/users/edit', to: 'users#edit', as: 'edit_user'
+  get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
+  patch '/users/:id', to: 'users#update', as: 'update_user'
+  put '/users/:id/edit', to: 'userss#update', as: 'updated_user'
   get 'login', to: 'sessions#new'
   resources :sessions, only: [:new, :create, :destroy]
   
