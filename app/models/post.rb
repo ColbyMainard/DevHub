736bee228102363d5d40ff8b5edf643603022d4f
EC2 Default User <ec2-user@ip-172-31-14-226.us-east-2.compute.internal>
2021-04-17 02:30:07 +0000

    find the posts whose title includes specific keyword

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,19 @@
 class Post < ApplicationRecord
     belongs_to :user, optional: true
     
-    def self.search(username)
+    def self.search_by_user(username)
         user = User.find_by_username(username)
         if user
             return Post.where(user_id: user)
         else
             return nil
         end
+    end
+    
+    def self.search_by_keyword(keyword)
+        # find the posts whose title includes the keyword
+        return Post.where("post_title like ?", "%#{keyword}%")
+        
+        # keyword in post description?
     end
 end
